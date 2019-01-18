26a299b4074c68989f9fab01f657b7bae7a42678
LPLaser <mlzxgzy@gmail.com>
2019-01-18 11:16:51 +0800

    R1.0 能正常工作了

--- pDNSPod.py
+++ pDNSPod.py
@@ -11,10 +11,9 @@ config_name = "./pDNSPod.json"
 config_json = {"Login": {"Account": {"enable": False, "username": "用户名", "password": "密码"},
                          "Token": {"enable": False, "ID": "5位ID", "Token": "Token"}}, "Domains": [
     {"enable": False, "domain": "域名", "sub_domain": "子域名，如是一级域名填@即可", "record_type": "记录类型，默认为A",
-     "record_line": "域名线路，默认或电信或联通等等，中文即可", "value": "记录内容"}]}
+     "record_line": "域名线路，默认或电信或联通等等，中文即可", "value": "记录内容,如需自动获取请输入auto"}]}
 
 post_data = {"format": "json",
-             #  "": "80155,1a767401f5a1f5219033699d93e01a7d",
              "lang": "cn"}
 domain_data = []
 
@@ -23,31 +22,65 @@ DDNSUpdate = "https://dnsapi.cn/Record.Ddns"
 RecordList = "https://dnsapi.cn/Record.List"
 
 
-def main():
-    if str(json.loads(requests.post(APIVersion, data=post_data).text)['status']['code']) != "1":
-        print("登录失败，请检查")
-        exit(1)
+def check_login():
+    ret = json.loads(requests.post(APIVersion, data=post_data).text)
+    if ret['status']['code'] == '1':
+        return True, ret['status']['message']
     else:
-        print('登录成功')
-    record_data = {'domain': "kdajv.com",
-                   "sub_domain": "@",
-                   "record_type": "A"}
-    record_id = str(json.loads(requests.post(RecordList, data={**record_data, **post_data}).text)['records'][0]['id'])
+        return False, ret['status']['message']
 
-    ddns_data = {'domain': 'kdajv.com',
-                 'sub_domain': '@',
-                 'value': '2.2.2.1',
-                 'record_line': '默认',
-                 "record_id": record_id}
-    ret = json.loads(requests.post(DDNSUpdate, data={**ddns_data, **post_data}).text)
-    if ret['status']['code'] == "1":
-        print("更新成功")
-        print("IP: %s" % ret['record']['value'])
-        exit(0)
-    else:
-        print("更新失败")
-        print(ret['status']['message'])
+
+def get_ID():
+    global domain_data
+    for tmp in domain_data:
+        ret = json.loads(requests.post(RecordList, data={**post_data, **tmp, 'length': '1'}).text, encoding='utf-8')
+        tmp['record_id'] = ret['records'][0]['id']
+        tmp['ovalue'] = ret['records'][0]['value']
+        pass
+
+
+def update_ddns():
+    for tmp in domain_data:
+        ret = json.loads(requests.post(DDNSUpdate, data={**tmp, **post_data}).text)
+        if ret['status']['code'] == "1":
+            print("更新成功")
+            print("IP: %s" % ret['record']['value'])
+            exit(0)
+        else:
+            print("更新失败")
+            print(ret['status']['message'])
+            exit(1)
+
+
+def process_value():
+    ip = requests.get("https://api.ip.sb/ip").text.strip()
+    for tmp in domain_data:
+        if tmp['value'] == "auto":
+            tmp['value'] = ip
+    for tmp in domain_data:
+        if tmp['value'] == tmp['ovalue']:
+            print("%s%s 记录未发生变化忽略" % (tmp['sub_domain'] is not "@" and tmp['sub_domain'] + "." or "", tmp['domain']))
+            domain_data.remove(tmp)
+        pass
+
+
+def main():
+    # Login
+    flg, msg = check_login()
+    if not flg:
+        print("!!! 登录失败")
+        print(msg)
         exit(1)
+    print("登录成功，服务版本 %s " % msg)
+    # Record ID
+    get_ID()
+    print('记录获取成功')
+    print("共 %s 个数据" % len(domain_data))
+    # Process Value
+    process_value()
+    # Update DDNS
+    update_ddns()
+    print("更新成功")
 
 
 def detect_config():
@@ -71,6 +104,7 @@ def detect_config():
             print("可能配置文件出现问题")
             print(e)
             exit(1)
+        print("配置读取完成")
 
 
 def read_config():
@@ -106,4 +140,4 @@ def read_config():
 
 if __name__ == "__main__":
     detect_config()
-    # main()
+    main()
